82dab3c4ba8d708f5a0c70bcb41d22532d91c41f
unknown <jamesenglajom@gmail.com>
2024-10-19 15:25:17 +0800

    form new ui initial attemp

--- src/pages/Inventory.jsx
+++ src/pages/Inventory.jsx
@@ -1,3 +1,59 @@
+import { useState } from "react";
+
 export function InventoryPage(){
-    return <h1>Inventory Page</h1>
+    
+    const [tabs, setTabs] = useState(()=>{
+        return [
+            {name:"specs", label: "Specifications"},
+            {name:"reefer", label: "Reefer"},
+            {name:"payment", label: "Payment"},
+            {name:"others", label: "Others"},
+        ];
+    })
+    const [tab, setTab] = useState("specs");
+
+
+    return <>
+    <h1>Temporary test page for UI Page</h1>
+    {/* wrapper */}
+    <div className="p-5 w-full">
+        <div className="">
+            <div className="flex items-center justify-between py-1 px-3">
+                <div>title</div>
+                <div>
+                    <button className="rounded-full h-[35px] w-[35px] bg-stone-50 border border-stone-300">x</button>
+                </div>
+            </div>
+            {/* form */}
+            <div className="flex w-full shadow-lg border border-stone-400">
+                {/* tabs */}
+                <div className="w-[150px]">
+                    {
+                        tabs.map((i,index)=>(
+                            <div onClick={()=> setTab(i.name)}key={`tab-${i.name}`} className={`${index < (tabs.length-1)? "border-b":``}  ${tab === i.name?"":"border-r"} border-stone-400`}>
+                                <div className={`p-3 ${tab === i.name?"":"text-stone-400"}`}>{i.label}</div>
+                            </div>
+                        ))
+                    }
+                </div>
+                {/* content wrap */}
+                <div className="p-4">
+                    {/* content */}
+                    {
+                        tab === "specs" && 
+                        <div>
+                            <div>
+                                <label htmlFor=""></label>
+                                <select name="" id=""></select>
+                            </div>
+                        </div>
+                    }
+                </div>
+            </div>
+            <div className="mt-5">
+                <button className="react-primary-button">Submit</button>
+            </div>
+        </div>
+    </div>
+    </>
 }
